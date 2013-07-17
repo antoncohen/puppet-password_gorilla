32f061047c59927ddd8d6815ccecd195e79498e5
Anton Cohen <anton@antoncohen.com>
2013-07-16 19:39:44 -0700

    Initial commit of Password Gorilla module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install Password Gorilla in /Applications
+#
+# include password_gorilla
+class password_gorilla {
+  package { 'password_gorilla':
+    source   => 'http://gorilla.dp100.com/downloads/gorilla15371.zip',
+    provider => 'compressed_app'
+  }
 }
